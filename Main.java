4cbdac0f9546cc519d58b721315c27efeda829a6
kostasgk <33001843+kostasgk@users.noreply.github.com>
2022-12-18 01:46:40 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(2596));
+    }
+
+    public static int getEvenDigitSum(int number){
+        if(number<0){
+            return -1 ;
+        } else {
+            int number_r = number , remain=0,sum=0;
+            while(number_r>0){
+                remain = number_r%10;
+                if(remain%2==0){
+                    sum = sum+remain;
+                }
+
+                number_r/=10;
+            }
+            return sum;
+        }
+    }
+
+}
+
